ff15c4e9b098b8847050ea0aed75332cad5de126
RoosveltK <roosvelt.kenne.9817@fasciences-uy1.cm>
2022-06-30 04:03:08 +0100

    finish with new twilo

--- src/main/java/com/servlet/checknote_final/SendSMS.java
+++ src/main/java/com/servlet/checknote_final/SendSMS.java
@@ -7,11 +7,16 @@ import com.twilio.type.PhoneNumber;
 public class SendSMS {
 	public static final String ACCOUNT_SID = "AC9be83ce7e83abd400d2336be823d4a54";
 	public static final String AUTH_TOKEN = "7b6a22b764d20657792e06e3f3ab712d";
+	String delano_key="+18125062855";
+	
 
+	static String phone_ken ="+19855099156";
+	public static final String ACCOUNT_SID_K = "AC4816ffd1fb2551bcd83ca216a31f5cf2";
+	public static final String AUTH_TOKEN_K = "c9997f83081ea70aa19696756c1a5ea3";
 	public static void sendSms(String number, String msg) {
-		Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+		Twilio.init(ACCOUNT_SID_K, AUTH_TOKEN_K);
 
-		Message message = Message.creator(new PhoneNumber(number), new PhoneNumber("+18125062855"), msg).create();
+		Message message = Message.creator(new PhoneNumber(number), new PhoneNumber(phone_ken), msg).create();
 	}
 }
 
